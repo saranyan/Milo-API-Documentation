a5ea870e8e35b70744c3b1d649301e160c16baea
Saranyan Vigraham <svigraham@paypal.com>
2011-12-13 14:41:48 -0600

    Intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiloTest::Application.config.secret_token = '36889eb55ae03e9c90140527c48167230ef1f6da78d732ffb63c19ac7271a311b15e3198acc440bfd04bef164075c7ef17077e6a9f39820ffc552abf4213db93'
